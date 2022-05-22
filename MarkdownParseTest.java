f5381056655c381c2ab1387445a16cb9899c7ce4
Tony <93945730+Gosui@users.noreply.github.com>
2022-05-22 14:42:50 -0700

    Report 4

--- MarkdownParseTest.java
+++ MarkdownParseTest.java
@@ -8,7 +8,6 @@ import java.util.ArrayList;
 import org.junit.*;
 
 public class MarkdownParseTest {
-
     @Test
     public void addition() {
         assertEquals(2, 2);
@@ -69,6 +68,78 @@ public class MarkdownParseTest {
         markdown = Files.readString(fileName);
         actualLinks = MarkdownParse.getLinks(markdown);
         expectedLinks = new ArrayList<>();
+        assertEquals(expectedLinks, actualLinks);
+    }
+
+    @Test
+    public void myTest1() throws IOException{
+        Path fileName = Path.of("report4-1.md");
+        String markdown = Files.readString(fileName);
+        ArrayList<String> actualLinks = MarkdownParse.getLinks(markdown);
+        ArrayList<String> expectedLinks = new ArrayList<>();
+        expectedLinks.add("`google.com");
+        expectedLinks.add("google.com");
+        expectedLinks.add("ucsd.edu");
+        assertEquals(expectedLinks, actualLinks);
+    }
+
+    @Test
+    public void myTest2() throws IOException{
+        Path fileName = Path.of("report4-2.md");
+        String markdown = Files.readString(fileName);
+        ArrayList<String> actualLinks = MarkdownParse.getLinks(markdown);
+        ArrayList<String> expectedLinks = new ArrayList<>();
+        expectedLinks.add("a.com");
+        expectedLinks.add("a.com(())");
+        expectedLinks.add("example.com");
+        assertEquals(expectedLinks, actualLinks);
+    }
+
+    @Test
+    public void myTest3() throws IOException{
+        Path fileName = Path.of("report4-3.md");
+        String markdown = Files.readString(fileName);
+        ArrayList<String> actualLinks = MarkdownParse.getLinks(markdown);
+        ArrayList<String> expectedLinks = new ArrayList<>();
+        expectedLinks.add("https://www.twitter.com");
+        expectedLinks.add("https://sites.google.com/eng.ucsd.edu/cse-15l-spring-2022/schedule");
+        expectedLinks.add("https://cse.ucsd.edu/");
+        assertEquals(expectedLinks, actualLinks);
+    }
+
+    @Test
+    public void otherTest1() throws IOException{
+        Path fileName = Path.of("report4-1.md");
+        String markdown = Files.readString(fileName);
+        ArrayList<String> actualLinks = MarkdownParseReport.getLinks(markdown);
+        ArrayList<String> expectedLinks = new ArrayList<>();
+        expectedLinks.add("`google.com");
+        expectedLinks.add("google.com");
+        expectedLinks.add("ucsd.edu");
+        assertEquals(expectedLinks, actualLinks);
+    }
+
+    @Test
+    public void otherTest2() throws IOException{
+        Path fileName = Path.of("report4-2.md");
+        String markdown = Files.readString(fileName);
+        ArrayList<String> actualLinks = MarkdownParseReport.getLinks(markdown);
+        ArrayList<String> expectedLinks = new ArrayList<>();
+        expectedLinks.add("a.com");
+        expectedLinks.add("a.com(())");
+        expectedLinks.add("example.com");
+        assertEquals(expectedLinks, actualLinks);
+    }
+
+    @Test
+    public void otherTest3() throws IOException{
+        Path fileName = Path.of("report4-3.md");
+        String markdown = Files.readString(fileName);
+        ArrayList<String> actualLinks = MarkdownParseReport.getLinks(markdown);
+        ArrayList<String> expectedLinks = new ArrayList<>();
+        expectedLinks.add("https://www.twitter.com");
+        expectedLinks.add("https://sites.google.com/eng.ucsd.edu/cse-15l-spring-2022/schedule");
+        expectedLinks.add("https://cse.ucsd.edu/");
         assertEquals(expectedLinks, actualLinks);
     }
 }
